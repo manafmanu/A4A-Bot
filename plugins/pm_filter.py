061736e402e6d0c60cfdd469d69659e1a29b6394
jdbot127 <87983735+jdbot127@users.noreply.github.com>
2022-12-03 10:42:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -718,7 +718,7 @@ async def auto_filter(client, msg, spoll=False):
         )
     else:
         await message.reply_photo(photo=random.choice(PICS), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-        cap = f"🎭𝐓𝐢𝐭𝐥𝐞 : <code>{search}</code>\n")
+        cap = f"🎭𝐓𝐢𝐭𝐥𝐞 : <code>{search}</code>\n"
     if imdb and imdb.get('poster'):
         try:
             hehe =  await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
