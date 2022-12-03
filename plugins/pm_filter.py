3463946e7ff8efd767303a36c6f67dd97905d8d3
jdbot127 <87983735+jdbot127@users.noreply.github.com>
2022-12-03 11:33:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -717,7 +717,8 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        fek = await message.reply_photo(photo=random.choice(PICS), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+        fek = await message.reply_photo(photo=random.choice(PICS), caption=cap[:1024], 
+                                reply_markup=InlineKeyboardMarkup(btn))
         cap = f"🎭𝐓𝐢𝐭𝐥𝐞 : <code>{search}</code>\n"
     if imdb and imdb.get('poster'):
         try:
