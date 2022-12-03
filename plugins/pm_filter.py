0448afc4a09f5114439224a7407a1d6d1fb4f4f7
jdbot127 <87983735+jdbot127@users.noreply.github.com>
2022-12-03 17:08:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -717,7 +717,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<i><b>🎭ᴛɪᴛʟᴇ :</i></b> <code>{search}</code>\n<i><b>🕹️ᴘᴏᴡᴇʀᴇᴅ ʙʏ :</i></b> <i>@FF_Series_Only</i>"
+        cap = f"🎭ᴛɪᴛʟᴇ : <code>{search}</code> \n 🕹️ᴘᴏᴡᴇʀᴇᴅ ʙʏ :<i>@FF_Series_Only"
     if imdb and imdb.get('poster'):
         try:
             hehe =  await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
