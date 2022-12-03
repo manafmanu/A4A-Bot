c115e29ee8243aff942f7d9f4ec8ae3f5745099a
jdbot127 <87983735+jdbot127@users.noreply.github.com>
2022-12-03 15:43:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -717,7 +717,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        await message.reply_photo(photo=random.choice(PICS), caption=MOVIE_TXT.format(mention=message.from_user.mention, query=message.text, greeting=None, group_name = f"[{message.chat.title}](t.me/{message.chat.username})" or f"[{message.chat.title}](t.me/{message.from_user.username})"), reply_markup=InlineKeyboardMarkup(buttons))
+        cap = f"<i><b>🎭ᴛɪᴛʟᴇ :</i></b> <code>{search}</code>\n<i><b>🕹️ᴘᴏᴡᴇʀᴇᴅ ʙʏ :</i></b> <i>@FF_Series Only</i>"
     if imdb and imdb.get('poster'):
         try:
             hehe =  await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
