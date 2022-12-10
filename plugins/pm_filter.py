2dff8adbc5ce7e2746a8231da5ead38e9ec679eb
jdbot127 <87983735+jdbot127@users.noreply.github.com>
2022-12-10 11:37:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -63,7 +63,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("𝘚𝘰𝘳𝘳𝘺..! 𝘖𝘯𝘭𝘺 𝘙𝘦𝘲𝘶𝘦𝘴𝘵𝘦𝘥 𝘗𝘦𝘳𝘴𝘰𝘯 𝘊𝘢𝘯 𝘈𝘤𝘤𝘦𝘴𝘴 𝘛𝘩𝘪𝘴 𝘉𝘶𝘵𝘵𝘰𝘯  𝘠𝘰𝘶 𝘊𝘢𝘯 𝘛𝘳𝘺 𝘛𝘰 𝘙𝘦-𝘴𝘦𝘢𝘳𝘤𝘩 𝘛𝘩𝘦 𝘔𝘰𝘷𝘪𝘦 𝘕𝘢𝘮𝘦..!", show_alert=True)
     try:
         offset = int(offset)
     except:
