16658c1ac0a7c30f4f977cf2bc796e08260b7c8a
manafmanu <86864557+manafmanu@users.noreply.github.com>
2022-12-11 12:50:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/jdbot127/url-series-bot /Eva
+  git clone https://github.com/manafmanu/A4A-url-series-bot /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
